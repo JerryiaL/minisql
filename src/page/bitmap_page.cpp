633e910aa40efcdd7b89390f489928c90fd26054
cxy <1121537392@qq.com>
2022-05-08 11:33:46 +0800

    修改关于FILEPATH和memset的错误

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -1,14 +1,15 @@
 #include "page/bitmap_page.h"
-#define FILEPATH "data"
 template<size_t PageSize>
+#define FILENAME "data"
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
+  int FILEPATH = open(FILENAME, O_RDWR);
   char PageBuffer[PageSize];
   lseek(FILEPATH, 0, SEEK_SET);
   read(FILEPATH, PageBuffer, PageSize);
   int index = *((int *)PageBuffer);
   if (index == 0) {
     index = lseek(FILEPATH, 0, SEEK_END)/PageSize;
-	memset(PageBuffer, 0, sizeof(char));
+	memset(PageBuffer, 0, PageSize * sizeof(char));
     int *allocated = (int *)PageBuffer + 1;
     *allocated = 1;
 	write(FILEPATH, PageBuffer, PageSize);
@@ -20,14 +21,14 @@ bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
     lseek(FILEPATH, 0, SEEK_SET);
     write(FILEPATH, PageBuffer, PageSize);
   }
-  
   page_offset = index;
-  
+  close(FILEPATH);
   return true;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
+  int FILEPATH = open(FILENAME, O_RDWR);
   char PageBuffer[PageSize];
   lseek(FILEPATH, 0, SEEK_SET);
   read(FILEPATH, PageBuffer, PageSize);
@@ -39,16 +40,19 @@ bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
   *index = page_offset;
   lseek(FILEPATH, 0, SEEK_SET);
   write(FILEPATH, PageBuffer, PageSize);
+  close(FILEPATH);
   return true;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
+  int FILEPATH = open(FILENAME, O_RDWR);
   char PageBuffer[PageSize];
   lseek(FILEPATH, page_offset * PageSize, SEEK_SET);
   read(FILEPATH, PageBuffer, PageSize);
   int allocated = *((int *)PageBuffer * +1);
   if (allocated == 0) return true;
+  close(FILEPATH);
   return false;
 }
 
