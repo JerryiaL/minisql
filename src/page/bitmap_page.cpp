9f928900c39b820220ba9bce2d54ef61d6b23d60
cxy <1121537392@qq.com>
2022-05-09 10:02:08 +0800

    本来对位图页的理解有误，重写代码

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -2,63 +2,47 @@
 template<size_t PageSize>
 #define FILENAME "data"
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  int FILEPATH = open(FILENAME, O_RDWR);
-  char PageBuffer[PageSize];
-  lseek(FILEPATH, 0, SEEK_SET);
-  read(FILEPATH, PageBuffer, PageSize);
-  int index = *((int *)PageBuffer);
-  if (index == 0) {
-    index = lseek(FILEPATH, 0, SEEK_END)/PageSize;
-	memset(PageBuffer, 0, PageSize * sizeof(char));
-    int *allocated = (int *)PageBuffer + 1;
-    *allocated = 1;
-	write(FILEPATH, PageBuffer, PageSize);
-  } else {
-    lseek(FILEPATH, index * PageSize, SEEK_SET);
-    read(FILEPATH, PageBuffer, PageSize);
-    int *allocated = (int *)PageBuffer + 1;
-    *allocated = 1;
-    lseek(FILEPATH, 0, SEEK_SET);
-    write(FILEPATH, PageBuffer, PageSize);
+  if (page_allocated_ == MAX_CHARS) {
+    return false;
   }
-  page_offset = index;
-  close(FILEPATH);
+  bytes[next_free_page_] = 1;
+  page_allocated_++;
+  page_offset = next_free_page_;
+  if (page_allocated_ < MAX_CHARS) {
+    for (int i = next_free_page_; i < MAX_CHARS; i++) {
+      if (bytes[i] == 0){
+        next_free_page_ = i;
+        break;
+	  }
+	}
+  }
   return true;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  int FILEPATH = open(FILENAME, O_RDWR);
-  char PageBuffer[PageSize];
-  lseek(FILEPATH, 0, SEEK_SET);
-  read(FILEPATH, PageBuffer, PageSize);
-  int *allocated = (int *)PageBuffer + 1;
-  *allocated = 0;
-  lseek(FILEPATH, page_offset * PageSize, SEEK_SET);
-  write(FILEPATH, PageBuffer, PageSize);
-  int *index = (int *)PageBuffer;
-  *index = page_offset;
-  lseek(FILEPATH, 0, SEEK_SET);
-  write(FILEPATH, PageBuffer, PageSize);
-  close(FILEPATH);
+  if (bytes[page_offset] == 0) return false;
+  bytes[page_offset] = 0;
+  page_allocated_--;
+  next_free_page_ = min(next_free_page_, page_offset);
   return true;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-  int FILEPATH = open(FILENAME, O_RDWR);
-  char PageBuffer[PageSize];
-  lseek(FILEPATH, page_offset * PageSize, SEEK_SET);
-  read(FILEPATH, PageBuffer, PageSize);
-  int allocated = *((int *)PageBuffer * +1);
-  if (allocated == 0) return true;
-  close(FILEPATH);
-  return false;
+  if (bytes[page_offset] == 0)
+    return true;
+  else
+    return false;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFreeLow(uint32_t byte_index, uint8_t bit_index) const {
-  return false;
+  int offset = byte_index * 8 + bit_index;
+  if (bytes[offset] == 0)
+    return true;
+  else
+    return false;
 }
 
 template
