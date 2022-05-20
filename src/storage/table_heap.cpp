5ecff6ab285ad5fac788860f8b447d5bda13615d
OhGaGaGaGa <171927821@qq.com>
2022-05-20 10:58:39 +0800

    Fix missing unpins

--- src/storage/table_heap.cpp
+++ src/storage/table_heap.cpp
@@ -6,15 +6,15 @@ bool TableHeap::InsertTuple(Row &row, Transaction *txn) {
   auto page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(page_id));
   if (page == nullptr) {
     return false;
-  } 
+  }
   page->WLatch();
   bool f = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
   page->WUnlatch();
-  buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);  
-  if( f == true){
+  buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
+  if (f == true) {
     return true;
-  }else{
-    while(page->GetNextPageId() !=  -1){
+  } else {
+    while (page->GetNextPageId() != -1) {
       page_id = page->GetNextPageId();
       page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(page_id));
       if (page == nullptr) {
@@ -23,28 +23,28 @@ bool TableHeap::InsertTuple(Row &row, Transaction *txn) {
       page->WLatch();
       bool f = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
       page->WUnlatch();
-      buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);  
-      if( f == true){
+      buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
+      if (f == true) {
         return true;
       }
     }
     page_id_t pre_id = page_id;
     page = reinterpret_cast<TablePage *>(buffer_pool_manager_->NewPage(page_id));
-    page->Init(page_id,pre_id,log_manager_,txn);
+    page->Init(page_id, pre_id, log_manager_, txn);
     page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(pre_id));
-    page->WLatch(); 
+    page->WLatch();
     page->SetNextPageId(page_id);
     page->WUnlatch();
     buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
     if (page == nullptr) {
       return false;
-    }//cout << page->GetNextPageId() << endl;
+    }
     page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(page_id));
-    page->WLatch(); 
-    f = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_); 
+    page->WLatch();
+    f = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
     page->WUnlatch();
     buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
-    return f;   
+    return f;
   }
 }
 
@@ -84,10 +84,15 @@ void TableHeap::ApplyDelete(const RowId &rid, Transaction *txn) {
   auto page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(rid.GetPageId()));
   assert(page != nullptr);
   // Step2: Delete the tuple from the page.
-  // page->WLatch(); *********************************** whether can add in?
+  page->WLatch();
   page->ApplyDelete(rid, txn, log_manager_);
-  // page->WUnlatch();
-  buffer_pool_manager_->DeletePage(page->GetTablePageId());
+  page->WUnlatch();
+  ddebug(page->GetPinCount());
+  bool Unpin_status = buffer_pool_manager_->UnpinPage(page->GetPageId(), true);
+  ddebug(page->GetPinCount());
+  bool Delete_status = buffer_pool_manager_->DeletePage(page->GetTablePageId());
+  ddebug(Unpin_status);
+  ddebug(Delete_status);
 }
 
 void TableHeap::RollbackDelete(const RowId &rid, Transaction *txn) {
@@ -107,31 +112,35 @@ void TableHeap::FreeHeap() {
 }
 
 bool TableHeap::GetTuple(Row *row, Transaction *txn) {
- // cout << "GET" << endl;
+  // cout << "GET" << endl;
   page_id_t page_id = row->GetRowId().GetPageId();
-  //cout << "get page id: " << page_id << endl;
+  // cout << "get page id: " << page_id << endl;
   auto page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(page_id));
   if (page == nullptr) {
     return false;
-  } 
+  }
   bool f;
   page->WLatch();
   f = page->GetTuple(row, schema_, txn, lock_manager_);
   page->WUnlatch();
   buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
- return f;
+  return f;
 }
 
-TableIterator TableHeap::Begin(Transaction *txn) { 
+TableIterator TableHeap::Begin(Transaction *txn) {
   auto page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(first_page_id_));
   auto rid = new RowId();
+  page->WLatch();
   page->GetFirstTupleRid(rid);
+  page->WUnlatch();
+  buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
   // LOG(INFO) << "begin: " << rid->GetPageId() << " " << rid->GetSlotNum() << std::endl;
-  // LOG(INFO) << "begin: " << Row(*rid).GetRowId().GetPageId() << " " << Row(*rid).GetRowId().GetSlotNum() << std::endl;
+  // LOG(INFO) << "begin: " << Row(*rid).GetRowId().GetPageId() << " " << Row(*rid).GetRowId().GetSlotNum() <<
+  // std::endl;
   return TableIterator(this, Row(*rid));
 }
 
-TableIterator TableHeap::End() { 
+TableIterator TableHeap::End() {
   auto rid = new RowId(INVALID_ROWID);
   // LOG(INFO) << "end: " << rid->GetPageId() << " " << rid->GetSlotNum() << std::endl;
   // LOG(INFO) << "end: " << Row(*rid).GetRowId().GetPageId() << " " << Row(*rid).GetRowId().GetSlotNum() << std::endl;
--- src/storage/table_iterator.cpp
+++ src/storage/table_iterator.cpp
@@ -1,49 +1,45 @@
-#include "common/macros.h"
 #include "storage/table_iterator.h"
-#include "storage/table_heap.h"
+#include "common/macros.h"
 #include "glog/logging.h"
+#include "storage/table_heap.h"
 
 TableIterator::TableIterator(TableHeap *table_heap, Row row) : table_heap_(table_heap) {
   row_ = new Row(row.GetRowId());
-  LOG(INFO) << "it construct: " << row_->GetRowId().GetPageId() << " " << row_->GetRowId().GetSlotNum() << std::endl;
+  // LOG(INFO) << "it construct: " << row_->GetRowId().GetPageId() << " " << row_->GetRowId().GetSlotNum() << std::endl;
 }
 
 TableIterator::TableIterator(const TableIterator &other) : table_heap_(other.table_heap_) {
   row_ = new Row(other.row_->GetRowId());
-  LOG(INFO) << "it copy: " << row_->GetRowId().GetPageId() << " " << row_->GetRowId().GetSlotNum() << std::endl;
+  // LOG(INFO) << "it copy: " << row_->GetRowId().GetPageId() << " " << row_->GetRowId().GetSlotNum() << std::endl;
 }
 
 TableIterator::~TableIterator() {}
 
-bool TableIterator::operator == (const TableIterator &itr) const {
-  if (table_heap_ == itr.table_heap_ && row_->GetRowId() == itr.row_->GetRowId()) 
-    return true;
+bool TableIterator::operator==(const TableIterator &itr) const {
+  if (table_heap_ == itr.table_heap_ && row_->GetRowId() == itr.row_->GetRowId()) return true;
   return false;
 }
 
-bool TableIterator::operator != (const TableIterator &itr) const {
-  return !(*this == itr);
-}
+bool TableIterator::operator!=(const TableIterator &itr) const { return !(*this == itr); }
 
-const Row &TableIterator::operator*() {
-  return *row_;
-}
+const Row &TableIterator::operator*() { return *row_; }
 
-Row *TableIterator::operator->() {
-  return row_;
-}
+Row *TableIterator::operator->() { return row_; }
 
 TableIterator &TableIterator::operator++() {
   RowId cur_rid = row_->GetRowId();
   RowId *next_rid = new RowId();
-  TablePage* page = reinterpret_cast<TablePage *>(table_heap_->buffer_pool_manager_->FetchPage(cur_rid.GetPageId()));
-  page->WLatch(); 
+  TablePage *page = reinterpret_cast<TablePage *>(table_heap_->buffer_pool_manager_->FetchPage(cur_rid.GetPageId()));
+  page->WLatch();
   page->GetNextTupleRid(cur_rid, next_rid);
   page->WUnlatch();
   table_heap_->buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
   if (next_rid->GetPageId() == INVALID_PAGE_ID) {
     auto next_page = reinterpret_cast<TablePage *>(table_heap_->buffer_pool_manager_->FetchPage(page->GetNextPageId()));
+    next_page->WLatch();
     next_page->GetFirstTupleRid(next_rid);
+    next_page->WUnlatch();
+    table_heap_->buffer_pool_manager_->UnpinPage(next_page->GetTablePageId(), true);
   }
   row_->SetRowId(*next_rid);
   return *this;
