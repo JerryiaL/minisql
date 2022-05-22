e3afd3fcdd16b0c7ee08dcc7fcff70504f5a6e1b
OhGaGaGaGa <171927821@qq.com>
2022-05-22 15:14:48 +0800

    Update b_plus_tree_page.cpp

--- src/page/b_plus_tree_page.cpp
+++ src/page/b_plus_tree_page.cpp
@@ -5,15 +5,17 @@
  * Page type enum class is defined in b_plus_tree_page.h
  */
 bool BPlusTreePage::IsLeafPage() const {
+  if (page_type_ == IndexPageType::LEAF_PAGE) 
+    return true;
   return false;
 }
 
 bool BPlusTreePage::IsRootPage() const {
-  return false;
+  return parent_page_id_ == INVALID_PAGE_ID;
 }
 
 void BPlusTreePage::SetPageType(IndexPageType page_type) {
-
+  page_type_ = page_type;
 }
 
 /*
@@ -21,26 +23,29 @@ void BPlusTreePage::SetPageType(IndexPageType page_type) {
  * page)
  */
 int BPlusTreePage::GetSize() const {
-  return 0;
+  return size_;
 }
 
 void BPlusTreePage::SetSize(int size) {
-
+  ASSERT(size <= max_size_, "size <= max_size_");
+  size_ = size;
 }
 
 void BPlusTreePage::IncreaseSize(int amount) {
-
+  ASSERT(size_ + amount <= max_size_, "size_ + amount <= max_size_");
+  size_ += amount;
 }
 
 /*
  * Helper methods to get/set max size (capacity) of the page
  */
 int BPlusTreePage::GetMaxSize() const {
-  return 0;
+  return max_size_;
 }
 
-void BPlusTreePage::SetMaxSize(int size) {
-
+void BPlusTreePage::SetMaxSize(int max_size) {
+  ASSERT(size_ <= max_size, "size_ <= max_size");
+  max_size_ = max_size;
 }
 
 /*
@@ -48,29 +53,29 @@ void BPlusTreePage::SetMaxSize(int size) {
  * Generally, min page size == max page size / 2
  */
 int BPlusTreePage::GetMinSize() const {
-  return 0;
+  return max_size_ >> 1;
 }
 
 /*
  * Helper methods to get/set parent page id
  */
 page_id_t BPlusTreePage::GetParentPageId() const {
-  return INVALID_PAGE_ID;
+  return parent_page_id_;
 }
 
 void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) {
-
+  parent_page_id_ = parent_page_id;
 }
 
 /*
  * Helper methods to get/set self page id
  */
 page_id_t BPlusTreePage::GetPageId() const {
-  return INVALID_PAGE_ID;
+  return page_id_;
 }
 
 void BPlusTreePage::SetPageId(page_id_t page_id) {
-
+  page_id_ = page_id;
 }
 
 /*
