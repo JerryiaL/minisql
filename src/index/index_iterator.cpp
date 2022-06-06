2ef952d706670fb3439b0ff9441fc506ac7a4c32
OhGaGaGaGa <171927821@qq.com>
2022-06-06 16:42:20 +0800

    b+ tree: Fix bugs in index iterator

--- src/index/index_iterator.cpp
+++ src/index/index_iterator.cpp
@@ -4,42 +4,62 @@
 
 INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE::IndexIterator(LeafPage* leaf, int index, BufferPoolManager* bpm) :
   leaf_(leaf), index_(index), bpm_(bpm) {
+  if (leaf) {
     bpm_->FetchPage(leaf_->GetPageId())->RLatch();
     bpm_->UnpinPage(leaf_->GetPageId(), false);
   }
+}
+
+// INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE::IndexIterator(const INDEXITERATOR_TYPE &other) :
+//   leaf_(other.leaf_), index_(other.index_), bpm_(other.bpm_) {}
 
 INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE::~IndexIterator() {
-  if (!leaf_) return;
-  bpm_->FetchPage(leaf_->GetPageId())->RUnlatch();
-  bpm_->UnpinPage(leaf_->GetPageId(), false);
-  bpm_->UnpinPage(leaf_->GetPageId(), false);
+  if (leaf_) {
+    bpm_->FetchPage(leaf_->GetPageId())->RUnlatch();
+    bpm_->UnpinPage(leaf_->GetPageId(), false);
+    bpm_->UnpinPage(leaf_->GetPageId(), false);
+  }
 }
 
 INDEX_TEMPLATE_ARGUMENTS const MappingType& INDEXITERATOR_TYPE::operator*() {
   ASSERT(!(
-    leaf_ == nullptr || 
+    leaf_ == nullptr ||
     (index_ == leaf_->GetSize() && leaf_->GetNextPageId() == INVALID_PAGE_ID)
-    ), "out of range");
+    ), "IndexIterator::operator*: out of range");
   return leaf_->GetItem(index_);
 }
 
 INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE& INDEXITERATOR_TYPE::operator++() {
+  ASSERT(leaf_, "IndexIterator::operator++: leaf_ cannot be nullptr. ");
   index_++;
-  if (index_ == leaf_->GetSize() && leaf_->GetNextPageId() != INVALID_PAGE_ID) {
-    page_id_t next_page_id = leaf_->GetNextPageId();
+  // This page is iterate over
+  if (index_ == leaf_->GetSize()) {
+    // Have next page
+    if (leaf_->GetNextPageId() != INVALID_PAGE_ID) {
+      page_id_t next_page_id = leaf_->GetNextPageId();
 
-    Page* page = bpm_->FetchPage(next_page_id);
-    ASSERT(page, "IndexIterator(operator++): all page are pinned");
+      Page* page = bpm_->FetchPage(next_page_id);
+      ASSERT(page, "IndexIterator(operator++): Cannot Fetch next_page_id");
 
-    page->RLatch();
-    bpm_->FetchPage(leaf_->GetPageId())->RUnlatch();
-    bpm_->UnpinPage(leaf_->GetPageId(), false);
-    bpm_->UnpinPage(leaf_->GetPageId(), false);
+      page->RLatch();
+      bpm_->FetchPage(leaf_->GetPageId())->RUnlatch();
+      bpm_->UnpinPage(leaf_->GetPageId(), false);
+      bpm_->UnpinPage(leaf_->GetPageId(), false);
 
-    LeafPage* next_leaf = reinterpret_cast<LeafPage *>(page->GetData());
-    assert(next_leaf->IsLeafPage());
-    index_ = 0;
-    leaf_ = next_leaf;
+      LeafPage* next_leaf = reinterpret_cast<LeafPage*>(page->GetData());
+      assert(next_leaf->IsLeafPage());
+      index_ = 0;
+      leaf_ = next_leaf;
+    }
+    // Havn't next page
+    else {
+      bpm_->FetchPage(leaf_->GetPageId())->RUnlatch();
+      bpm_->UnpinPage(leaf_->GetPageId(), false);
+      bpm_->UnpinPage(leaf_->GetPageId(), false);
+
+      index_ = 0;
+      leaf_ = nullptr;
+    }
   }
   return *this;
 }
@@ -52,12 +72,12 @@ INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE INDEXITERATOR_TYPE::operator++(int) 
 
 INDEX_TEMPLATE_ARGUMENTS
 bool INDEXITERATOR_TYPE::operator==(const IndexIterator& itr) const {
-  return false;
+  return bpm_ == itr.bpm_ && leaf_ == itr.leaf_ && index_ == itr.index_;
 }
 
 INDEX_TEMPLATE_ARGUMENTS
 bool INDEXITERATOR_TYPE::operator!=(const IndexIterator& itr) const {
-  return false;
+  return bpm_ != itr.bpm_ || leaf_ != itr.leaf_ || index_ != itr.index_;
 }
 
 template
