689cb3a42641b22d4257e05c63823d7e8782c315
JerryiaL <89286434+JerryiaL@users.noreply.github.com>
2022-06-11 18:02:53 +0800

    update row for serialize suitable for key(32)
    b+_tree_test command for key(32)
    and so just modify it, but spend more time

--- src/record/row.cpp
+++ src/record/row.cpp
@@ -2,32 +2,57 @@
 
 uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
   // should be magic num here?
-  size_t count = GetFieldCount();
+  uint32_t count = GetFieldCount();
   uint32_t offset = 0;
 
   MACH_WRITE_TO(RowId, buf + offset, GetRowId());
   offset += sizeof(RowId);
 
-  MACH_WRITE_TO(size_t, buf + offset, count);
-  offset += sizeof(size_t);
+  MACH_WRITE_TO(uint32_t, buf + offset, count);
+  offset += sizeof(uint32_t);
 
-  for(size_t i = 0; i < count; i++){
+  uint32_t offset_field = 0;
+  for(uint32_t i = 0; i < count; i++){
     Field* f = GetField(i);
-    if(f == NULL)MACH_WRITE_TO(bool, buf + offset, 0);
-    else MACH_WRITE_TO(bool, buf + offset, 1);
-    offset += sizeof(bool);
+    if(f->IsNull())
+    {
+      MACH_WRITE_TO(bool, buf + offset + i, 1);
+      const TypeId type_id = (*(schema->GetColumn(i))).GetType();
+      if(type_id == kTypeInt)
+      {
+        MACH_WRITE_TO(char, buf + offset + count * sizeof(bool) + offset_field, '1');
+      }
+      else if (type_id == kTypeFloat)
+      {
+        MACH_WRITE_TO(char, buf + offset + count * sizeof(bool) + offset_field, '2');
+      }
+      else
+      {
+        MACH_WRITE_TO(char, buf + offset + count * sizeof(bool) + offset_field, '3');
+      }
+      offset_field += 1;
+    }
+    else
+    {
+      MACH_WRITE_TO(bool, buf + offset + i, 0);
+      const TypeId type_id = (*(schema->GetColumn(i))).GetType();
+      if(type_id == kTypeInt)
+      {
+        MACH_WRITE_TO(char, buf + offset + count * sizeof(bool) + offset_field, '1');
+      }
+      else if (type_id == kTypeFloat)
+      {
+        MACH_WRITE_TO(char, buf + offset + count * sizeof(bool) + offset_field, '2');
+      }
+      else
+      {
+        MACH_WRITE_TO(char, buf + offset + count * sizeof(bool) + offset_field, '3');
+      }
+      offset_field += 1;
+      offset_field += (*f).SerializeTo(buf + offset + count * sizeof(bool) + offset_field);
+    }
   }
-
-  for(size_t i = 0; i < count; i++){
-    Field* f = GetField(i);
-    if(f == NULL)continue;
-    const TypeId type_id = (*(schema->GetColumn(i))).GetType();
-    MACH_WRITE_TO(TypeId, buf + offset, type_id);
-    offset += sizeof(TypeId);
-
-    offset += (*f).SerializeTo(buf+offset);
-  }
-
+  offset = offset + count * sizeof(bool) + offset_field;
   return offset;
 }
 
@@ -37,38 +62,45 @@ uint32_t Row::DeserializeFrom(char *buf, Schema *schema) {
 
   SetRowId(MACH_READ_FROM(RowId, buf + offset));
   offset += sizeof(RowId);
-  size_t count = MACH_READ_FROM(size_t, buf + offset);
-  offset += sizeof(size_t);
-  std::vector<bool> NULLV;
+  uint32_t count = MACH_READ_FROM(uint32_t, buf + offset);
+  offset += sizeof(uint32_t);
+  uint32_t offset_field = 0;
   std::vector<Field* > tempfields;
-  for(size_t i = 0; i < count; i++){
-    bool tempB = MACH_READ_FROM(bool, buf + offset);
-    offset += sizeof(bool);
-    NULLV.push_back(tempB);
-  }
-
-  for(size_t i = 0; i < count; i++){
-    if(NULLV[i] == false)tempfields.push_back(NULL);
-    else{
-      TypeId type_id = MACH_READ_FROM(TypeId, buf + offset);
-      offset += sizeof(TypeId);
-      Field *tempf;
-      offset += (*tempf).DeserializeFrom(buf + offset, type_id, &tempf, 0, heap_);
-      tempfields.push_back(tempf);
+  for(size_t i = 0; i < count; i++)
+  {
+    bool tempB = MACH_READ_FROM(bool, buf + offset + i);
+    Field *tempf;
+    char type = MACH_READ_FROM(char, buf + offset + count * sizeof(bool) + offset_field);
+    TypeId this_type;
+    if(type == '1')
+    {
+      this_type = kTypeInt;
     }
+    else if(type == '2')
+    {
+      this_type = kTypeFloat;
+    }
+    else
+    {
+      this_type = kTypeChar;
+    }
+    offset_field += 1;
+    offset_field += Field::DeserializeFrom(buf + offset + count * sizeof(bool) + offset_field, this_type, &tempf, tempB, heap_);
+    tempfields.push_back(tempf);
   }
 
   fields_ = tempfields;
+  offset = offset + count * sizeof(bool) + offset_field;
+
   return offset;
 }
 
 uint32_t Row::GetSerializedSize(Schema *schema) const {
-  size_t count = GetFieldCount();
-  uint32_t offset = sizeof(RowId) + sizeof(size_t) + count*sizeof(bool);
+  uint32_t count = GetFieldCount();
+  uint32_t offset = sizeof(RowId) + sizeof(uint32_t) + count * sizeof(bool);
   for(size_t i = 0; i < count; i++){
     Field* f = GetField(i);
-    if(f == NULL)continue;
-    offset += (*f).GetSerializedSize() + sizeof(TypeId);
+    offset += (*f).GetSerializedSize() + sizeof(char);
   }
   return offset;
 }
