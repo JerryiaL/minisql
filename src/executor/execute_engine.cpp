d2cb44caa5023023169676fc4e67b40fac7176fe
OhGaGaGaGa <171927821@qq.com>
2022-06-19 09:52:53 +0800

    execute_engine: Update getAllDatabase()

--- src/executor/execute_engine.cpp
+++ src/executor/execute_engine.cpp
@@ -7,7 +7,7 @@
 #include <time.h>
 #include <chrono>
 #include <dirent.h>
-#include <sys/stat.h> 　
+#include <sys/stat.h>
 #include <sys/types.h> 
 
 void getAllDatabase(string path, vector<string>& files) 
@@ -22,7 +22,13 @@ void getAllDatabase(string path, vector<string>& files)
   int i = 0;
   while((dirp = readdir(dp)) != NULL){
     i++;
-    if(i > 2) files.push_back(dirp->d_name);
+    if (
+      (dirp->d_name[0] == '.' && dirp->d_name[1] == '\0') || 
+      (dirp->d_name[0] == '.' && dirp->d_name[1] == '.' && dirp->d_name[2] == '\0')
+    )
+      continue;
+    else 
+      files.push_back(dirp->d_name);
   }
 }
 
@@ -36,8 +42,7 @@ ExecuteEngine::ExecuteEngine()
 {
   vector<string> dbname;
   getAllDatabase("./database/", dbname);
-  for(int i = 0; i < dbname.size(); i++)
-  {
+  for (size_t i = 0; i < dbname.size(); i++) {
     DBStorageEngine * newdb = new DBStorageEngine("./database/"+dbname[i], false);
     dbs_.insert({dbname[i], newdb});
   }
